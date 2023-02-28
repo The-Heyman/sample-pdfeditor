b65125ae24b16e2f297f731f43cd0c7bbce5e81d
Santhosh Adiga U <sadiga80@gmail.com>
2023-02-28 21:48:57 +0530

    download fix - WIP
    1) Applied Clean Architecture
    2) Reformatted code and refactored methods
    3) Created download util

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:dio/dio.dart';
-import 'dart:io';
+
+import 'app/app.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,80 +12,5 @@ Future main() async {
       debug: true // optional: set false to disable printing logs to console
       );
   await Permission.storage.request();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late InAppWebViewController webView;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(children: <Widget>[
-          Expanded(
-              child: InAppWebView(
-            initialUrlRequest: URLRequest(
-                url: Uri.parse(
-                    "https://editor-8f407.web.app")),
-            initialOptions: InAppWebViewGroupOptions(
-              crossPlatform: InAppWebViewOptions(useOnDownloadStart: true),
-            ),
-            onWebViewCreated: (InAppWebViewController controller) {
-              webView = controller;
-            },
-            onDownloadStartRequest:
-                (InAppWebViewController inAppWebViewController,
-                    DownloadStartRequest downloadStartRequest) async {
-              var dio = Dio();
-              var dir = await getApplicationDocumentsDirectory();
-              var file = File(
-                  "${dir.path}/${downloadStartRequest.suggestedFilename}");
-              print(file);
-              var url = downloadStartRequest.url.path;
-              var response = await dio.download(
-                url,
-                file.path,
-                onReceiveProgress: (received, total) {
-                  // Print the download progress
-                  print("$received");
-                },
-              );
-              if (response.statusCode == 200) {
-                print("File Downloaded");
-              }
-            },
-            /*async {
-                print("downloading");
-                print(downloadStartRequest.url.toString());
-                final taskId = await FlutterDownloader.enqueue(
-                  url: downloadStartRequest.url.toString(),
-                  savedDir:
-                      (await getApplicationDocumentsDirectory())!.path,
-                  showNotification:
-                      true, // show download progress in status bar (for Android)
-                  openFileFromNotification:
-                      true, // click on notification to open downloaded file (for Android)
-                );
-              }*/
-          ))
-        ]),
-      ),
-    );
-  }
+  runApp(const App(),);
 }
---
+++ lib/utils/download.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> downloadFile(String url, [String? filename]) async {
+  var hasStoragePermission = await Permission.storage.isGranted;
+  Directory downloadDir = Directory('/storage/emulated/0/Download');
+  if (!hasStoragePermission) {
+    final status = await Permission.storage.request();
+    hasStoragePermission = status.isGranted;
+  }
+  if (hasStoragePermission) {
+    final taskId = await FlutterDownloader.enqueue(
+        url: url,
+        headers: {},
+        savedDir: downloadDir.path,
+        saveInPublicStorage: true,
+        fileName: filename);
+  }
+}
